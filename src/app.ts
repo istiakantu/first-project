087eccabec3e965b49714c83aef64137f449d4b4
istiakantu <istakahmed636@gmail.com>
2024-05-27 01:34:20 +0600

    student controller, route & service added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { clear } from 'console';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -8,8 +8,14 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// api router
+
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   res.send('Hello');
-});
+};
+
+app.get('/', getAController);
 
 export default app;
